b305fc3c65f511effa05fe486cd0ef4dc2b8c339
kudii <78762072+Kuddii@users.noreply.github.com>
2021-04-04 15:07:26 -0500

    Create dog.js

---
+++ modules/animal/dog.js
@@ -0,0 +1,27 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
+
+module.exports = class animals extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'dog',
+            group: 'animals',
+            memberName: 'dog',
+            description: 'dog',
+        })
+    }
+    run(msg) {
+
+        axios
+            .get('https://dog.ceo/api/breeds/image/random')
+            .then((res) => {
+                ('RES', res.data.message)
+
+                const doggo = new MessageEmbed()
+                    .setImage(res.data.message)
+
+                msg.channel.send(doggo)
+            })
+    }
+}
