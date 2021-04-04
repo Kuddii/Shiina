dea523648c5bfed4d444ff87990c915ffd7e45d0
kudii <78762072+Kuddii@users.noreply.github.com>
2021-04-04 12:51:20 -0500

    Update Shiina.js

--- Shiina.js
+++ Shiina.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { CommandClient, SQLiteProvider } = require('discord.js-commando');
 const { PREFIX, UWUOWO, KEIKOV2, INVITELOL } = process.env;
+const sqlite3 = require('sqlite3');
+const sqlite = require('sqlite');
 
 const client = new CommandoClient({
 	commandPrefix: PREFIX,
@@ -9,3 +11,10 @@ const client = new CommandoClient({
 	disableMentions: 'everyone',
 	unknownCommandResponse: false,
 });
+
+sqlite.open({ filename: 'kamikaze.db', driver: sqlite3.Database }).then((db) => {
+	client.setProvider(new SQLiteProvider(db));
+});
+
+client.on("warn", (e) => console.warn(e));
+client.on("error", (e) => console.error(e));
