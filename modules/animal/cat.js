f4b46c6e8f2a5dd97d0174c90fd43a8302b7ce0a
kudii <78762072+Kuddii@users.noreply.github.com>
2021-04-04 13:07:30 -0500

    Update cat.js

--- modules/animal/cat.js
+++ modules/animal/cat.js
@@ -1,1 +1,28 @@
+const { Command } = require('discord.js-commando');
+const { MessageEmbed } = require('discord.js');
+const axios = require('axios');
 
+module.exports = class animals extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'cat',
+            group: 'animals',
+            memberName: 'cat',
+            description: 'cat',
+        })
+    }
+  run(msg) {
+    
+    axios
+        .get('https://api.thecatapi.com/v1/images/search')
+        .then((res) => {
+        ('RES', res.data.url)
+      
+        const UwU = new MessageEmbed()
+            .setColor('RANDOM')
+            .setImage(res.data.url)
+
+        msg.channel.send(UwU)
+    })
+  }
+}
